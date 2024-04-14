6a1aa0dc092e4f6c467b61b87d1e47a881a27fa9
Janidu Pasan Hewapathirana <138660521+jpasan123@users.noreply.github.com>
2024-04-14 19:58:21 +0530

    update commit

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const connectDatabase=()=>{
-    mongoose.connect("mongodb://localhost:27017").then(con=>{
+    mongoose.connect("mongodb://localhost:27017/").then(con=>{
         console.log(`MongoDB is connected to the host ${con.connection.host}`)
     })
 }
